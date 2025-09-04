819d562eef7af4051f05bcfaad030b309113e70c
Nonmajor <znfhtkzl2002@naver.com>
2025-09-05 07:30:50 +0900

    [fix] 씬 전환 시 슬라이더 미작동 오류 수정

--- Assets/2 Scripts/AudioManager.cs
+++ Assets/2 Scripts/AudioManager.cs
@@ -1,9 +1,8 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI; // 추가: UI 요소를 사용하기 위해 필요
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
@@ -19,24 +18,17 @@ public class AudioManager : MonoBehaviour
     public AudioSource flashlightSoundSource;
     public AudioSource footstepSoundSource;
 
-    // === 추가: 볼륨 조절 슬라이더 변수 ===
-    [Header("Volume Sliders")]
-    public Slider mainVolumeSlider;
-    public Slider musicVolumeSlider;
-    public Slider effectVolumeSlider;
+    // 이 변수들은 이제 값을 저장하는 역할만 합니다.
+    private float mainVolume = 1.0f;
+    private float musicVolume = 1.0f;
+    private float effectVolume = 1.0f;
 
-
-    // 원래 볼륨을 저장할 변수들
+    // 원래 볼륨 저장 변수들은 유지
     private float mainMusicOriginalVolume;
     private float ingameMusicOriginalVolume;
     private float chaseMusicOriginalVolume;
     private float flashlightOriginalVolume;
     private float footstepOriginalVolume;
-
-    // 현재 볼륨 상태를 저장할 변수
-    private float mainVolume = 1.0f;
-    private float musicVolume = 1.0f;
-    private float effectVolume = 1.0f;
 
     private void Awake()
     {
@@ -51,38 +43,31 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         }
 
-        // Awake()에서 모든 AudioSource의 볼륨을 1로 강제 설정 후, 원래 볼륨 값 저장
+        // Awake에서 원래 볼륨을 저장합니다.
+        // 이 값들은 OnSceneLoaded에서 다시 설정되지 않습니다.
         if (mainMusicSource != null)
         {
-            mainMusicSource.volume = 1.0f;
             mainMusicOriginalVolume = mainMusicSource.volume;
         }
         if (ingameMusicSource != null)
         {
-            ingameMusicSource.volume = 1.0f;
             ingameMusicOriginalVolume = ingameMusicSource.volume;
         }
         if (chaseMusicSource != null)
         {
-            chaseMusicSource.volume = 1.0f;
             chaseMusicOriginalVolume = chaseMusicSource.volume;
         }
         if (flashlightSoundSource != null)
         {
-            flashlightSoundSource.volume = 1.0f;
             flashlightOriginalVolume = flashlightSoundSource.volume;
         }
         if (footstepSoundSource != null)
         {
-            footstepSoundSource.volume = 1.0f;
             footstepOriginalVolume = footstepSoundSource.volume;
         }
-    }
 
-
-    private void Update()
-    {
-        UpdateAllVolumes();
+        // 게임 시작 시 저장된 볼륨 설정 불러오기
+        LoadVolumeSettings();
     }
 
     private void OnDestroy()
@@ -92,6 +77,9 @@ public class AudioManager : MonoBehaviour
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        // 씬이 로드될 때마다 새로운 슬라이더를 찾아서 볼륨을 설정합니다.
+        FindAndSetupVolumeSliders();
+
         if (scene.name == "MainMenu")
         {
             PlayMainMusic();
@@ -104,10 +92,79 @@ public class AudioManager : MonoBehaviour
         {
             StopAllMusic();
         }
+    }
 
+    private void FindAndSetupVolumeSliders()
+    {
+        // 씬의 모든 Slider 컴포넌트를 찾습니다.
+        Slider[] allSliders = FindObjectsOfType<Slider>(true);
+
+        foreach (Slider slider in allSliders)
+        {
+            if (slider.gameObject.name == "MainVolumeSlider")
+            {
+                slider.value = mainVolume;
+                slider.onValueChanged.RemoveAllListeners();
+                slider.onValueChanged.AddListener(SetMainVolume);
+            }
+            else if (slider.gameObject.name == "MusicVolumeSlider")
+            {
+                slider.value = musicVolume;
+                slider.onValueChanged.RemoveAllListeners();
+                slider.onValueChanged.AddListener(SetMusicVolume);
+            }
+            else if (slider.gameObject.name == "EffectVolumeSlider")
+            {
+                slider.value = effectVolume;
+                slider.onValueChanged.RemoveAllListeners();
+                slider.onValueChanged.AddListener(SetEffectVolume);
+            }
+        }
+    }
+
+    // PlayerPrefs에 볼륨 설정을 저장하는 함수
+    private void SaveVolumeSettings()
+    {
+        PlayerPrefs.SetFloat("MainVolume", mainVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.SetFloat("EffectVolume", effectVolume);
+        PlayerPrefs.Save();
+    }
+
+    // PlayerPrefs에서 볼륨 설정을 불러오는 함수
+    private void LoadVolumeSettings()
+    {
+        mainVolume = PlayerPrefs.GetFloat("MainVolume", 1.0f); // 기본값 1.0f
+        musicVolume = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
+        effectVolume = PlayerPrefs.GetFloat("EffectVolume", 1.0f);
+
+        // 불러온 볼륨 값으로 모든 사운드 볼륨을 즉시 업데이트
         UpdateAllVolumes();
     }
 
+    // 각 슬라이더의 onValueChanged 이벤트에 연결할 함수들
+    public void SetMainVolume(float volume)
+    {
+        mainVolume = volume;
+        UpdateAllVolumes();
+        SaveVolumeSettings(); // 볼륨 변경 시 저장
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicVolume = volume;
+        UpdateAllVolumes();
+        SaveVolumeSettings();
+    }
+
+    public void SetEffectVolume(float volume)
+    {
+        effectVolume = volume;
+        UpdateAllVolumes();
+        SaveVolumeSettings();
+    }
+
+    // === 기존 함수들 ===
     private void StopAllMusic()
     {
         if (mainMusicSource != null) mainMusicSource.Stop();
@@ -181,37 +238,13 @@ public class AudioManager : MonoBehaviour
         if (footstepSoundSource != null) footstepSoundSource.UnPause();
     }
 
-    public void SetMainVolume(float volume)
-    {
-        mainVolume = volume;
-        UpdateAllVolumes();
-    }
-
-    public void SetMusicVolume(float volume)
-    {
-        musicVolume = volume;
-        UpdateAllVolumes();
-    }
-
-    public void SetEffectVolume(float volume)
-    {
-        effectVolume = volume;
-        UpdateAllVolumes();
-    }
-
     private void UpdateAllVolumes()
     {
-        // 슬라이더가 연결되어 있으면 값을 가져오고, 아니면 기본값(1.0f) 사용
-        float currentMainVolume = (mainVolumeSlider != null) ? mainVolumeSlider.value : 1.0f;
-        float currentMusicVolume = (musicVolumeSlider != null) ? musicVolumeSlider.value : 1.0f;
-        float currentEffectVolume = (effectVolumeSlider != null) ? effectVolumeSlider.value : 1.0f;
-
-        if (mainMusicSource != null) mainMusicSource.volume = mainMusicOriginalVolume * currentMainVolume * currentMusicVolume;
-        if (ingameMusicSource != null) ingameMusicSource.volume = ingameMusicOriginalVolume * currentMainVolume * currentMusicVolume;
-        if (chaseMusicSource != null) chaseMusicSource.volume = chaseMusicOriginalVolume * currentMainVolume * currentMusicVolume;
-
-        if (flashlightSoundSource != null) flashlightSoundSource.volume = flashlightOriginalVolume * currentMainVolume * currentEffectVolume;
-        if (footstepSoundSource != null) footstepSoundSource.volume = footstepOriginalVolume * footstepOriginalVolume * currentMainVolume * currentEffectVolume;
+        if (mainMusicSource != null) mainMusicSource.volume = mainMusicOriginalVolume * mainVolume * musicVolume;
+        if (ingameMusicSource != null) ingameMusicSource.volume = ingameMusicOriginalVolume * mainVolume * musicVolume;
+        if (chaseMusicSource != null) chaseMusicSource.volume = chaseMusicOriginalVolume * mainVolume * musicVolume;
+        if (flashlightSoundSource != null) flashlightSoundSource.volume = flashlightOriginalVolume * mainVolume * effectVolume;
+        if (footstepSoundSource != null) footstepSoundSource.volume = footstepOriginalVolume * mainVolume * effectVolume;
     }
 
     private IEnumerator StopChaseMusicWithFade(float fadeOutDuration, float delayDuration)
